a02ad331b9b59e02ad85bdb356e565260cd49ef4
MrClassic1011 <pravinubale497@gmail.com>
2024-04-28 03:17:49 -0700

    Python | Count and display vowels in a string

--- stringProblems/problem_3.py
+++ stringProblems/problem_3.py
@@ -1,1 +1,26 @@
 # Python | Count and display vowels in a string
+
+def Check_Vow(string, vowels):
+    final = [each for each in string if each in vowels]
+    print(len(final))
+    print(final)
+    
+
+string = "Pravin is learning very fast"
+vowels = "AaEeIiOoUu"
+Check_Vow(string, vowels)
+
+# Counting vowels: Dictionary Way
+
+def Check_Vow1(string, vowels):
+    count = {}.fromkeys(vowels, 0)
+    
+    for character in string:
+        if character in vowels:
+            count[character] += 1
+            print(character, count[character])
+    return count
+
+print(Check_Vow1(string, vowels))
+
+# Counting vowels: recursive function way
